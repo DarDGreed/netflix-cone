f88fd743e1386297421ba1c7407ed84970ebff6b
ShadyDar <141896824+DarDGreed@users.noreply.github.com>
2025-05-09 20:56:31 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://netflix-cone-frontend.onrender.com'],
+  origin: 'https://netflix-cone-frontend.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
